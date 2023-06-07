ff931e201b36b98af91a0bc0e390095b4af96e28
jewmin <jewmin@gmail.com>
2023-06-07 22:05:03 +0800

    修改盛宴活动

--- task/activity/GoldGiftType.py
+++ task/activity/GoldGiftType.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 from task.activity.ActivityTask import ActivityTask
 from protocol import *
+from functools import cmp_to_key
 
 
 class GoldGiftType(ActivityTask):
@@ -13,6 +14,29 @@ class GoldGiftType(ActivityTask):
         if not self.Enable():
             return self.next_half_hour
 
+        nation = self.account.user.nation
+        def helper(x, y):
+            key = {
+                1: "weinum",
+                2: "shunum",
+                3: "wunum",
+            }
+            xx, yy = x[key[nation]], y[key[nation]]
+            if x["bufnum"] > y["bufnum"]:
+                return -1
+            elif x["bufnum"] < y["bufnum"]:
+                return 1
+            elif xx > yy:
+                return -1
+            elif xx < yy:
+                return 1
+            elif x["finishnum"] > y["finishnum"]:
+                return -1
+            elif x["finishnum"] < y["finishnum"]:
+                return 1
+            else:
+                return 0
+
         info = await kfBanquet.kfBanquet(self.account)
         if info is not None:
             if info["基础点券"] > 0:
@@ -22,8 +46,9 @@ class GoldGiftType(ActivityTask):
                 return self.immediate * 5
             elif info["状态"] == 1:
                 if info["加入盛宴免费次数"] > 0:
+                    info["盛宴房间"] = sorted(info["盛宴房间"], key=cmp_to_key(helper))
                     for room in info["盛宴房间"]:
-                        if room["bufnum"] > 0 and room["nation"] == self.account.user.nation:
+                        if room["nation"] == self.account.user.nation:
                             await kfBanquet.joinBanquet(self.account, room=room["rank"], playername=room["playername"])
                             return self.immediate
                 elif info["加入盛宴花费金币"] <= self.GetConfig("buyjoingold") and info["加入盛宴花费金币"] <= self.GetAvailableGold():
